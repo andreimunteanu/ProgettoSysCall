20d42c329e122f6d7383812ba6629f351660026a
niccolomarastoni <eightnine@gmail.com>
2013-06-05 18:53:10 +0200

    Update pcalc.h

--- pcalc.h
+++ pcalc.h
@@ -16,12 +16,17 @@ typedef struct operation{
   float res;
 }operation;
 
+typedef operation* op_addr;
+
 int sem_id;
 int mem_id;
 
 int read_integer(int fd);
-void init_sh_mem(key_t *mem_key, operation **operations ,int lines);
-int is_operator(char c);
-void read_remaining_line(int fd, operation *cursor);
-void write_line(int fd, operation *cursor);
+void init_sh_mem(key_t *mem_key, operation **operations ,op_addr** addresses,int lines,int n_proc);
 void copy_operations(int fd, int *proc_id, operation **operations,int lines);
+void start(int sem_id, const int sem_num, op_addr *address, const int AVAILABLE_WORKERS, const int REMAINING_WORK);
+void P(int sem_num);
+void V(int sem_num);
+void child_P(const int sem_num1, int sem_num2);
+void child_finish(const int sem_num1, int sem_num2 );
+void init_sem(key_t *sem_key, int n_proc, int lines);
