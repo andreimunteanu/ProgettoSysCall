c0b2bdfa1671e0244ba85bfb62fab4fbf6dfd90b
niccolomarastoni <eightnine@gmail.com>
2013-06-20 22:32:55 +0200

    Update pcalc.c

--- pcalc.c
+++ pcalc.c
@@ -1,64 +1,5 @@
 #include "pcalc.h"
 
-float *get_results(operation *operations,int lines){
-  int i;
-  float *temp = (float*)malloc(lines*sizeof(float));
-  for(i = 0; i < lines ; ++i){
-    *(temp+i) = (operations+i*sizeof(operation))->res;
-  }
-  return temp;
-}
-
-int get_sem_val(int sem_num){
-  union semun {
-    int val;
-    struct semid_ds * buffer;
-    unsigned short * array;
-  } arg;
-  
-  semctl(sem_id, sem_num, GETVAL, arg);
-  return arg.val;
-}
-
-void print_to_file(int fd, char *string, int len){
-  int n;
-
-  if((n = write(fd, string, len)) == -1){
-    perror("write");
-    exit(1);
-  }
-}
-
-int count_lines(int fd){
-  int chars, n, lines;
-  char buf[1];
-  chars = 0;
-  lines = -1;
-  while((n = read(fd,buf, 1)) > 0){//controllo per la sintassi
-    chars++;
-    if(*buf == '\n')
-      lines++;
-  }	
-  
-  lseek(fd, -chars, SEEK_END);
-  return lines;
-}
-
-char *prompt_user(char *msg){
-  int i = 0;
-  char *filename = malloc(64*sizeof(char));
-  char buf[1];
-  //char *str = "Inserisci nome file di configurazione: ";
-  write(1, msg, strlen(msg));
-  fflush(stdout);
-  while((read(2, buf, 1) > 0) && *buf != '\n'){
-    filename[i++] = buf[0];
-  }
-  filename[i] = '\0';
-  i = 0;	
-  return filename;
-}
-
 void main(int argc, char *argv[]){
   int fd,n_proc,j,i = 0, lines = -1;  // da dichiarare i register
   int available_workers, remaining_work; 
@@ -69,6 +10,7 @@ void main(int argc, char *argv[]){
   key_t mem_key1,mem_key2, sem_key;
   pid_t status;
   int id;
+  char buf[64];
   char *conf_file;
   char *filename;
   
@@ -77,59 +19,33 @@ void main(int argc, char *argv[]){
   }
   else{
     if(argc == 1){
-      conf_file = prompt_user("insert file name: ");
+      conf_file = prompt_user("Insert file name: ");
     }
     
     else
       conf_file = argv[1];
     
-    if((fd = open(conf_file,O_RDONLY, S_IRUSR)) == -1){
-      perror("open conf");
-      exit(1);
-    }
+    if((fd = open(conf_file,O_RDONLY, S_IRUSR)) == -1)
+      syserr("fd");
     
     if(argc == 1)
       free(conf_file);
-
+    
     lines = count_lines(fd);    
     n_proc = read_integer(fd);
+    sprintf(buf, "Main process is spawning %d child%s for %d operations.\n",n_proc,(n_proc-1)?"ren":"",lines - 1);
+    print_to_video(buf);
     available_workers = 2*n_proc;
     remaining_work = 2*n_proc +1;
     proc_ids = (int *)malloc(lines * sizeof(int));    
 	
     init_sh_mem(&mem_key1,&mem_key2, &operations,&addresses,lines,n_proc);
-    
-    //printf("in ops %p\n",operations);
     copy_operations(fd, proc_ids,  &operations,lines);
-    //printf("last %p \n", addresses + (n_proc-1)*sizeof(op_addr));
-    
-       
-    for(i = 0; i<lines; ++i){
-      j = i*sizeof(operation);
-      //printf("%d %c %d\n",(operations +j)->num1, (operations+j)->op,(operations +j)->num2);
-    }
-
     init_sem(&sem_key, n_proc, lines);
-    
-    union semun{
-      int val;
-      struct semid_ds * buffer;
-      unsigned short * array;
-    }arg;
-    arg.array = (u_short *)malloc((2*n_proc + 2) * sizeof(u_short)); 
-    semctl(sem_id, 0, GETALL, arg);
-   
-    /*
-      for(i = 0;i < 2*n_proc + 2;++i){
-      printf("%d ", arg.array[i] );
-      }      
-      printf("\n");*/
 
     for(i = 0;i < n_proc;++i){
-      if((status = fork()) == -1){
-	perror("fork");
-	exit(1);
-      }
+      if((status = fork()) == -1)
+	syserr("fork");
 
       if(status == 0){
 	break;
@@ -137,17 +53,12 @@ void main(int argc, char *argv[]){
     }
     
     if(status == 0){
-      /* if((addresses = (op_addr*) shmat(mem_id, NULL, 0)) == (void *)-1){
-	 perror("shmat");
-	 exit(1);
-	 }
-	 addresses = addresses;*/
-      //printf();
+ 
       start(sem_id, 2*i,  addresses + i*sizeof(op_addr), available_workers, remaining_work);
-      //      printf("Esco \n");
+     
       exit(0);
     }
-    //    printf("tocca al babbo\n");      
+  
     for(i= 0; i < lines; ++i){
       id = (proc_ids[i]-1);
       
@@ -155,8 +66,10 @@ void main(int argc, char *argv[]){
 	P(available_workers);
 	V(available_workers);
 	j = 0;
-	while(get_sem_val(j=(j+1) % n_proc) == 0){
-	  printf(" %d non disp \n",j);
+	while(get_sem_val((2*j+1)) == 0){
+	  //printf(" %d non disp \n",j+1);
+	  j = (j +1)%n_proc;
+	  //printf("provo %d \n",j+1);
 	}
 	id = j;
       }
@@ -188,7 +101,6 @@ void main(int argc, char *argv[]){
       V(2*i);
     }
     
-    printf("");
     for(i = 0; i < n_proc; ++i)
       wait();
     //printf("Ciao papa, muore.\n");
